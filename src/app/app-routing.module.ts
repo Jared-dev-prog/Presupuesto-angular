5777568ef7a784ae024a86c745c79c418b4d23ff
Jared <jared.trejo@axity.com>
2023-04-14 14:32:49 -0600

    Budget HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'ingresar-presupuesto',
+    pathMatch: 'full',
+  },
+  {
+    path: 'ingresar-presupuesto',
+    component: IngresarPresupuestoComponent,
+  },
+  {
+    path: 'gastos',
+    component: GastosComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'ingresar-presupuesto',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
