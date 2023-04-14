1c45f857f8e4f216791abfac225317dd03851f74
Jared <jared.trejo@axity.com>
2023-04-14 15:05:36 -0600

    Crear componente gasto HTML

--- src/app/components/gastos/gastos.component.ts
+++ src/app/components/gastos/gastos.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css']
+  styleUrls: ['./gastos.component.css'],
 })
 export class GastosComponent {
-
+  constructor(
+    public _presupuestoServie: PresupuestoService,
+    private router: Router
+  ) {
+    if (this._presupuestoServie.presupuesto <= 0) {
+      this.router.navigate(['/ingresar-presupuesto']);
+    }
+  }
 }
