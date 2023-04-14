201131d4d9bc9d7d1a753fcd627cfb1e246e566d
Jared <jared.trejo@axity.com>
2023-04-14 14:18:13 -0600

    starting development app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { CrearGastoComponent } from './components/gastos/crear-gasto/crear-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresarPresupuestoComponent,
+    GastosComponent,
+    CrearGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gastos/crear-gasto/crear-gasto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-gasto',
+  templateUrl: './crear-gasto.component.html',
+  styleUrls: ['./crear-gasto.component.css']
+})
+export class CrearGastoComponent {
+
+}
