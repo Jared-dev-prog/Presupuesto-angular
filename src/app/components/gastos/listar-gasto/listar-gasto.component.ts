3953015782eec54e440dbe015b3b88f33c05273c
Jared <jared.trejo@axity.com>
2023-04-14 16:18:59 -0600

    Finalizacion app

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
-  styleUrls: ['./listar-gasto.component.css']
+  styleUrls: ['./listar-gasto.component.css'],
 })
 export class ListarGastoComponent {
+  presupuesto: number = 0;
+  restante: number = 0;
+  listaGastos: any[] = [];
 
+  constructor(private _presupuestoService: PresupuestoService) {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+
+    this._presupuestoService.getGastos().subscribe((data) => {
+      this.restante = this.restante - data.cantidad;
+      this.listaGastos.push(data);
+    });
+  }
 }
--- src/app/services/presupuesto.service.ts
+++ src/app/services/presupuesto.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +8,16 @@ export class PresupuestoService {
   presupuesto: number = 0;
   restante: number = 0;
 
+  private gastos$ = new Subject<any>();
+
   constructor() {}
 
   agregarGasto(gasto: any): void {
     this.restante = this.restante - gasto.cantidad;
+    this.gastos$.next(gasto);
+  }
+
+  getGastos(): Observable<any> {
+    return this.gastos$.asObservable();
   }
 }
